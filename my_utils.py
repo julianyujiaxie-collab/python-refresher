06a20110391edac96d6e7b148f7c301f29f4330b
Julian Xie <julian.yujia.xie@gmail.com>
2026-08-29 17:19:25 -0600

    Implement get_column for CSV value extraction

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,11 @@
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    column_values = []
+
+    with open(file_name, "r") as file:
+        for line in file:
+            values = line.strip().split(",")
+
+            if values[query_column] == query_value:
+                column_values.append(values[result_column])
+
+    return column_values
