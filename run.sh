a1dca9bf6b8ad206e6b383dafa063cf5e6a71d09
Julian Xie <julian.yujia.xie@gmail.com>
2026-09-06 12:55:22 -0600

    Update run.sh to demostrate two errors

--- run.sh
+++ run.sh
@@ -2,4 +2,26 @@
 
 set -e
 cd -- "${0:A:h}"
-python print_fires.py
+
+print -r -- "1. Valid query"
+python3 print_fires.py \
+    --country "United States of America" \
+    --country-column 0 \
+    --fires-column 3 \
+    --file-name "Agrofood_co2_emission.csv"
+
+print
+print -r -- "2. File-opening error"
+python3 print_fires.py \
+    --country "United States of America" \
+    --country-column 0 \
+    --fires-column 3 \
+    --file-name "missing.csv"
+
+print
+print -r -- "3. Integer-conversion error"
+python3 print_fires.py \
+    --country "Area" \
+    --country-column 0 \
+    --fires-column 3 \
+    --file-name "Agrofood_co2_emission.csv"
